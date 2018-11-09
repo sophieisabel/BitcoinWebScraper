7aaea9f6ccaa5a2a89cbb5aabc07c8e861656eba
Sophie Reich-Michalik <sophie.reich-michalik@mail.mcgill.ca>
2018-11-09 12:04:16 +0100

    Indent and parenthesis fixes

--- bitcoinWebScraper.py
+++ bitcoinWebScraper.py
@@ -1,3 +1,5 @@
+# coding: utf-8
+# Here your code !
 #Sophie Reich-Michalik
 #Cite: https://medium.freecodecamp.org/how-to-scrape-websites-with-python-and-beautifulsoup-5946935d93fe
 
@@ -10,26 +12,26 @@ import csv
 from datetime import datetime
 
 #define the url
-pageurl = ‘http://bitcointalk.org'
+pageurl = 'http://bitcointalk.org'
 
 #query the website and define the html to 'page'
 page = urllib2.urlopen(pageurl)
 
 # parse the html using beautiful soup and store in variable `parsedpage`
-parsedpage = BeautifulSoup(page, ‘html.parser’)
+parsedpage = BeautifulSoup(page, 'html.parser')
 
 #variable 'parsedpage' contains the HTML of the page
 
-content = parsedpage.find(‘td’, attrs={‘class’: ‘windowbg2’})
+content = parsedpage.find('td', attrs={'class': 'windowbg2'})
 cont = content.text.strip() # strip() is used to remove starting and trailing
-print cont
+print (cont)
 
 # get the index name
-namebox = parsedpage.find(‘div’, attrs={‘class’:’catbg’})
+namebox = parsedpage.find('div', attrs={'class':'catbg'})
 name = namebox.text
-print name
+print (name)
 
 # Write to excel file
-with open(‘index.csv’, ‘a’) as csv_file:
-writer = csv.writer(csv_file)
-writer.writerow([name, price, datetime.now()])
+with open('index.csv', 'a') as csv_file:
+    writer = csv.writer(csv_file)
+    writer.writerow([name, price, datetime.now()])
